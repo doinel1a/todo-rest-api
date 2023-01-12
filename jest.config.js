0619439de7165c046d5fd6a03ca8099d6ef42516
doinel1a <doinel1atanasiu@gmail.com>
2023-01-12 20:36:04 +0100

    Jest configured

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable unicorn/prefer-module */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	testMatch: ['**/**/*.test.ts'],
+	verbose: true,
+	forceExit: true
+};
