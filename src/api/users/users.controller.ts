d1d464ac0fb49ef12f442cbdbad3eb722aa6a736
doinel1a <doinel1atanasiu@gmail.com>
2023-01-15 18:23:05 +0100

    Users controller refactored to handle User input

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,20 +1,29 @@
 import { Request, Response } from 'express';
+import { MongooseError } from 'mongoose';
 
 import log from '../../utils/logger';
+import { CreateUserInput } from './user.schema';
 import createUser from './users.service';
 
-async function createUserController(request: Request, response: Response) {
+async function createUserController(
+	request: Request<
+		Record<string, never>,
+		Record<string, never>,
+		CreateUserInput['body']
+	>,
+	response: Response
+) {
 	try {
 		const user = await createUser(request.body);
 
 		return response.status(201).send(user);
 	} catch (_error: unknown) {
-		const error = _error as Error;
+		const error = _error as MongooseError;
 
 		log.error('!!! CREATING USER ERROR !!!');
 		log.error(`ERROR MESSAGE: ${error.message}`);
 
-		return response.status(409).send(error.message);
+		return response.status(409).send({ error: `${error.message}` });
 	}
 }
 
